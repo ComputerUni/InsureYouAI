98639a9d5f6dd8f8d46d7247da69253c57a71cd4
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-07-26 10:07:41 +0300

    updated: refactor default page sections into ViewComponents (head, header contact, navbar, mobile menu)

---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultSliderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
--- InsureYouAI/obj/Debug/net9.0/InsureYouAI.AssemblyInfo.cs
+++ InsureYouAI/obj/Debug/net9.0/InsureYouAI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb752b5ea8526d67bdd7ba4441594f6cdcc964c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64c007b02ccaba4e0c253c249832188f389e91b")]
 [assembly: System.Reflection.AssemblyProductAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
