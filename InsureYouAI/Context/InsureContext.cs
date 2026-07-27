003e3e50e82dadf5a911eab21065fda0138d23f5
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-07-27 16:12:24 +0300

    updated: add PricingPlan and PricingPlanItem tables with relational structure and integrate into Default page via ViewComponent

--- InsureYouAI/Context/InsureContext.cs
+++ InsureYouAI/Context/InsureContext.cs
@@ -23,5 +23,6 @@ namespace InsureYouAI.Context
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<TrailerVideo> TrailerVideos { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<PricingPlanItem> PricingPlanItems { get; set; }
     }
 }
--- InsureYouAI/obj/Debug/net9.0/InsureYouAI.AssemblyInfo.cs
+++ InsureYouAI/obj/Debug/net9.0/InsureYouAI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98639a9d5f6dd8f8d46d7247da69253c57a71cd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b25698f4190a421a43b15dde0ff059de42a98f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
