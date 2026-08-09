a61fc65ff5eab6e841617a78cf27690359c69050
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-08-10 00:00:41 +0300

    Poliçe tahminleme

---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DashboardForecastingPoliciesSalesComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardForecastingPoliciesSalesComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
--- InsureYouAI/obj/Debug/net9.0/InsureYouAI.AssemblyInfo.cs
+++ InsureYouAI/obj/Debug/net9.0/InsureYouAI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9bfbd84f641ad7d10cf5a66dcb9b3b38e3d56cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a9a820fbcee7a7c11f4d96026c2ed1e8ba1ca8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InsureYouAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
